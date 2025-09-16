85a904b7fa36c2ab6e2a8f63c60fe33f79c67a24
Rugsit <rugsit.nest@gmail.com>
2025-09-16 19:36:13 +0700

    fix: rebase conflict in server/main.c

--- cores/server/main.c
+++ cores/server/main.c
@@ -12,11 +12,8 @@
 
 int main()
 {
-int main()
-{
     int sock;
     struct message msg;
-    struct sockaddr_in server, client;
     struct sockaddr_in server, client;
     unsigned int client_len = sizeof(client);
 
@@ -39,27 +36,8 @@ int main()
         perror("Error : port is in used");
         exit(EXIT_FAILURE);
     }
-    switch (sock)
-    {
-    case ERR_SOCKET_FAIL:
-    {
-        perror("Error : failed to create socket");
-        exit(EXIT_FAILURE);
-    }
-    case ERR_INVALID_PORT:
-    {
-        perror("Error : port is invalid");
-        exit(EXIT_FAILURE);
-    }
-    case ERR_PORT_IN_USE:
-    {
-        perror("Error : port is in used");
-        exit(EXIT_FAILURE);
-    }
     }
 
-    while (1)
-    {
     while (1)
     {
         memset(&msg, 0, sizeof(msg));
@@ -71,7 +49,6 @@ int main()
         }
 
         segment_file(msg.data, sock, client);
-        
     }
 
     return 0;
